d58694f89c0080185d09aa20a0c4c188841eb1e4
Greg Holmes <greg.holmes2@vonage.com>
2020-11-18 14:51:29 +0000

    Added usage of environment variables in send-sms and send-sms-unicode code examples

--- sms/send-sms-unicode.go
+++ sms/send-sms-unicode.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/vonage/vonage-go-sdk"
 )
 
 func main() {
-	var from string = VONAGE_BRAND_NAME
-	var to string = TO_NUMBER
+	var from string = os.Getenv("VONAGE_BRAND_NAME")
+	var to string = os.Getenv("TO_NUMBER")
 	var text string = "こんにちは世界"
 
-	auth := vonage.CreateAuthFromKeySecret(API_KEY, API_SECRET)
+	auth := vonage.CreateAuthFromKeySecret(os.Getenv("VONAGE_API_KEY"), os.Getenv("VONAGE_API_SECRET"))
 	smsClient := vonage.NewSMSClient(auth)
 	response, err := smsClient.Send(from, to, text, vonage.SMSOpts{Type: "unicode"})
 
