379b466fafa9c0e97869141ff75ce9f25c7aa24b
Lorna Jane Mitchell <lorna@lornajane.net>
2020-12-24 14:36:55 +0000

    Add snippets for basic/standard/advanced number insights API

---
+++ number-insight/advanced.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/vonage/vonage-go-sdk"
+)
+
+func main() {
+	godotenv.Load("../.env")
+
+	auth := vonage.CreateAuthFromKeySecret(os.Getenv("VONAGE_API_KEY"), os.Getenv("VONAGE_API_SECRET"))
+	niClient := vonage.NewNumberInsightClient(auth)
+
+	result, _, _ := niClient.AdvancedAsync("447770007777", "https://example.com/number-insight-data", vonage.NiOpts{})
+	if result.Status == 0 {
+		http.HandleFunc("/number-insight-data", func(w http.ResponseWriter, r *http.Request) {
+			data, _ := ioutil.ReadAll(r.Body)
+			fmt.Println(string(data))
+		})
+
+		http.ListenAndServe(":8080", nil)
+	} else {
+		fmt.Println("Request status " + string(result.Status) + ": " + result.StatusMessage)
+	}
+}
